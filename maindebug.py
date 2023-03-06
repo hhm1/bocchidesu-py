79a262a5e40ae02006ab51ac91af20fd897dff2e
haoming9245 <haoming9245@outlook.com>
2023-03-06 10:30:34 +0800

    Add files via upload

---
+++ maindebug.py
@@ -0,0 +1,18 @@
+import bocchidesu
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyMainWindow(QMainWindow, bocchidesu.Ui_Bocchidesu):
+
+    def __init__(self, parent=None, layout=None, main_window=None):
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWin = MyMainWindow()
+    myWin.show()
+    sys.exit(app.exec_())
